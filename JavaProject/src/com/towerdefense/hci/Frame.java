3151324f63b99a4259d40b9a5528354423d57f64
Pierre-Antoine Fagniez <pierreantoine.fagniez@viacesi.fr>
2015-06-03 19:01:59 +0200

    fentre

--- JavaProject/src/com/towerdefense/hci/Frame.java
+++ JavaProject/src/com/towerdefense/hci/Frame.java
@@ -1,13 +1,26 @@
 package com.towerdefense.hci;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class Frame extends JFrame {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private final Panel contentPane;
+
 	public Frame (){
+		this.contentPane = new Panel();
+		this.contentPane.setBackground(Color.BLACK);
+		this.contentPane.setLayout(new BorderLayout());
+
 
 		setSize(750, 600);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		setContentPane(this.contentPane);
 	}
 }
