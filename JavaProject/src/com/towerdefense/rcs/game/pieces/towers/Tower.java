8f04b656f33ea2bdcf8b08fcef97c9313cde3e4f
PONTACQ <PONTACQ@Alex_PC>
2015-06-06 22:16:47 +0200

    dfgw

--- JavaProject/src/com/towerdefense/rcs/game/pieces/towers/Tower.java
+++ JavaProject/src/com/towerdefense/rcs/game/pieces/towers/Tower.java
@@ -1,17 +1,24 @@
 package com.towerdefense.rcs.game.pieces.towers;
 
+import java.awt.Image;
+
 import com.towerdefense.rcs.game.Upgradable;
+import com.towerdefense.rcs.game.pieces.DamageType;
 import com.towerdefense.rcs.game.pieces.Fighter;
 
 public class Tower extends Fighter implements Upgradable{
+	
+	protected int level;
+	protected int attackSpeed;
+	protected DamageType damagetype;
+	protected int attackRange;
+	protected int health;
+	protected Image image;
+	protected int x;
+	protected int y;
+	
 
-	@Override
-	public boolean upgrade() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public void move() {
+	public Tower() {
 	}
 		public boolean die(){
 			return false;
@@ -22,6 +29,12 @@ public class Tower extends Fighter implements Upgradable{
 			return attackRange;
 			
 		}
+		@Override
+		public boolean upgrade() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	
 
 }
 	
