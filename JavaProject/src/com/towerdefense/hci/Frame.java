282959cf99ff342c5a96062742833d0e8d0df423
Pierre-Antoine Fagniez <pierreantoine.fagniez@viacesi.fr>
2015-06-08 09:17:06 +0200

    f

--- JavaProject/src/com/towerdefense/hci/Frame.java
+++ JavaProject/src/com/towerdefense/hci/Frame.java
@@ -15,8 +15,8 @@ public class Frame extends JFrame {
 	private GamePanel gamePanel;
 
 	public Frame (){
-		//buildNameSelectionPanel();
-		buildMap1();
+		buildNameSelectionPanel();
+		//buildMap1();
 		build();
 	}
 
--- JavaProject/src/com/towerdefense/rcs/game/pieces/towers/TowerFactory.java
+++ JavaProject/src/com/towerdefense/rcs/game/pieces/towers/TowerFactory.java
@@ -1,4 +1,24 @@
 package com.towerdefense.rcs.game.pieces.towers;
 
+
 public class TowerFactory {
+
+	public TowerFactory() {
+		//factory();
+	}
+
+	/*public static Tower factory ()
+	{
+		final ArrayList<Tower> arbres = new ArrayList<Tower>();
+
+		arbres.add(new Pommier());
+		arbres.add(new Chene());
+		arbres.add(new Peuplier());
+		arbres.add(new Sapin());
+
+		final int index = (int) (Math.random() * arbres.size());
+
+		return arbres.get(index);
+	}*/
+
 }
